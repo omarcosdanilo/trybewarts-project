41b16b6b9fd1f76a22e93e9078a748941596e6ca
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-11 16:38:12 -0300

    Requisito 3 ok

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificaLogin() {
+  const email = document.querySelector('#input-email');
+  const senha = document.querySelector('#input-senha');
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+const myBtn = document.querySelector('#btn-login');
+myBtn.addEventListener('click', verificaLogin);
